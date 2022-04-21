9be77ea285172eb490b763bb8a483bc0b4d9fa1b
tenick <usareskenneth22@gmail.com>
2022-04-22 01:20:54 +0800

    Added mongodb functionality using Mongoose, created user model; Added registration

--- app.js
+++ app.js
@@ -1,39 +1,62 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('./model/User');
 
 const app = express();
 
-// config
+const DBUri = ''; // request kenneth for URI
+mongoose.connect(DBUri)
+.then(result => app.listen(8888))
+.catch(err => console.log(err));
+
+// config / middlewares
 app.set('view engine', 'ejs');
 
 app.use('/css', express.static('css'));
 app.use('/assets', express.static('assets'));
 app.use('/scripts', express.static('scripts'));
 
+app.use(express.urlencoded({extended: true}));
 
 // routes
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About Us'});
 });
 
 app.get('/plants', (req, res) => {
-    res.render('plants');
+    res.render('plants', {title: 'Plants'});
 });
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login', {title: 'Login'});
 });
 
 app.get('/register', (req, res) => {
-    res.render('register')
+    res.render('register', {title: 'Register'});
+});
+
+app.post('/register', (req, res) => {
+    res.send('ADDED TO DATABASE<br>'+JSON.stringify(req.body));
+    const user = new User({
+        first_name: req.body['first_name'],
+        last_name: req.body['last_name'],
+        email: req.body['email'],
+        password: req.body['password']
+    });
+    user.save()
+    .then(result => {
+
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
 
 app.get('/forgot_password', (req, res) => {
-    res.render('forgot_password')
+    res.render('forgot_password', {title: 'Forgot Password'});
 });
-
-// start listening
-app.listen(8888);
