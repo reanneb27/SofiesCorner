7272d5359fbdef5a22c16f3038cdd54acf84618e
tenick <usareskenneth22@gmail.com>
2022-05-04 21:31:46 +0800

    added flash messages for registration

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session'); 
+const flash = require('connect-flash');
 
 const User = require('./model/User');
 
 const app = express();
 
-const DBUri = ''; // request kenneth for URI
+const DBUri = 'mongodb+srv://admin:admin@cluster0.mh5yi.mongodb.net/sofies-corner?retryWrites=true&w=majority'; // request kenneth for URI
 mongoose.connect(DBUri)
 .then(result => app.listen(8888))
 .catch(err => console.log(err));
@@ -18,6 +20,15 @@ app.use('/assets', express.static('assets'));
 app.use('/scripts', express.static('scripts'));
 
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({ 
+    secret: 'woot',
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.use(flash());
+
 
 // routes
 app.get('/', (req, res) => {
@@ -33,30 +44,46 @@ app.get('/plants', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.render('login', {title: 'Login'});
+    const flashMsg = req.flash('success');
+
+    res.render('login', {title: 'Login', flashObj: {flashType: 'error', message: flashMsg}});
 });
 
 app.get('/register', (req, res) => {
-    res.render('register', {title: 'Register'});
+    const flashMsg = req.flash('error');
+    
+    res.render('register', {title: 'Register', flashObj: {flashType: 'error', message: flashMsg}});
 });
 
 app.post('/register', (req, res) => {
-    res.send('ADDED TO DATABASE<br>'+JSON.stringify(req.body));
     const user = new User({
         first_name: req.body['first_name'],
         last_name: req.body['last_name'],
         email: req.body['email'],
         password: req.body['password']
     });
+
+    // check first if email already exists
+    
+
     user.save()
     .then(result => {
-
+        req.flash('success', 'Successfully Registered! You can login now.');
+        res.redirect('/login');
     })
     .catch(err => {
         console.log(err);
+        req.flash('error', 'There was an error with your registration.');
+        res.redirect('/register');
     });
 });
 
 app.get('/forgot_password', (req, res) => {
     res.render('forgot_password', {title: 'Forgot Password'});
+});
+
+app.get('/test', (req, res) => {
+    console.log(req.session);
+    req.flash('flashMsg', 'boyow');
+    res.send('braaah test');
 });
